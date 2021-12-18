4def954ecf439ce8fcec101f64588730bb1a4e2b
alireza.soltani@cafebazaar.ir <nprZQAGhSz4E4Lf!>
2021-12-18 17:06:18 +0330

    Add some comments

--- emulator_runner.py
+++ emulator_runner.py
@@ -16,17 +16,20 @@ def get_emulator_list():
     # Get the list of all emulators installed
     with Popen([emulator_command, get_emulator_list_command], shell=True, stdout=PIPE) as proc:
         for val in proc.stdout.readlines()[1:-1]:
+            # Convert byte to string
             device_name = val.decode('UTF-8').strip()
             emulator_list.append(device_name)
 
 
 def create_emulator_button():
     for emulator_device in emulator_list:
+        # Create button with some attributes - fg=text color/text=button text/command(optional)
         button = tk.Button(frame,
                            text=emulator_device,
                            fg="black")
         # Bind keys to use them with event
         root.bind('<Button>', on_click)
+        # Put the buttons from left to right and next to each other
         button.pack(side=tk.LEFT)
 
 
